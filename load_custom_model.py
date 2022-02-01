d40191e30e7bc134bd1cab64f3d11b08ceaa1994
Suphi C Altintasli <43508611+scaltintasli@users.noreply.github.com>
2022-01-31 22:28:06 -0600

    add comment reminder

--- load_custom_model.py
+++ load_custom_model.py
@@ -5,6 +5,8 @@ from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
 from object_detection.utils import config_util
 
+# Always check custom model name and update it if it is not correct
+# And Check label_map.pbtxt too, make sure it has right object labels
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
 PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
